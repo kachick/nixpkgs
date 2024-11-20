b4f1a5f85467b3d866c04d7c03d500b70b030242
Kenichi Kamiya <kachick1@gmail.com>
2024-11-20 20:53:17 +0900

    killport: 0.9.2 -> 1.1.0

--- pkgs/tools/misc/killport/default.nix
+++ pkgs/tools/misc/killport/default.nix
@@ -5,16 +5,28 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "killport";
-  version = "0.9.2";
+  version = "1.1.0";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-eyRI4ZVp9HPMvpzyV9sQdh2r966pCdyUPnEhxGkzH3Q=";
+    hash = "sha256-7bENyg/KR4oI//jvG6bw+3UX3j9ITAXCMTpc+65VBZ8=";
   };
 
-  cargoHash = "sha256-QQ43dT9BTu7qCzpnTGKzlVL6jKDXofXStYWYNLHSuVs=";
+  cargoHash = "sha256-KKlY1c70wYaEgYQGveUf4mfYuZ81fAQHVANKniw4nL4=";
 
   nativeBuildInputs = [ rustPlatform.bindgenHook ];
+
+  preCheck = ''
+    export RUST_BACKTRACE=full
+  '';
+
+  # checkFlags = [
+  #   # assertion failed: re.is_match(data)
+  #   "--skip=tests::test_dry_run_option"
+  #   "--skip=tests::test_basic_kill_process"
+  #   "--skip=tests::test_signal_handling"
+  #   "--skip=tests::test_mode_option"
+  # ];
 
   meta = with lib; {
     description = "Command-line tool to easily kill processes running on a specified port";
