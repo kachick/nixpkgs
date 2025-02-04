1c6529bb307a066c0f34bd147771a1a74077387a
Kenichi Kamiya <kachick1@gmail.com>
2025-02-04 09:12:19 +0900

    brush: 0.2.14 -> 0.2.15

--- pkgs/by-name/br/brush/package.nix
+++ pkgs/by-name/br/brush/package.nix
@@ -3,22 +3,38 @@
   rustPlatform,
   fetchFromGitHub,
   stdenv,
+  versionCheckHook,
+  # nix-update-script,
+  brush,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "brush";
-  version = "0.2.14";
+  version = "0.2.15";
 
   src = fetchFromGitHub {
     owner = "reubeno";
     repo = "brush";
     tag = "brush-shell-v${version}";
-    hash = "sha256-g9ZvatbNDWlPya3rWjt120LPhp4Io6KHTWXlMddm4zs=";
+    hash = "sha256-hPF2nXYXAM+5Lz2VJw9vZ6RFZ40y+YkO94Jc/sLUYsg=";
   };
 
   useFetchCargoVendor = true;
 
-  cargoHash = "sha256-SLlIC847GEM5/0cX9PdLFtNaZMMSwUBhaKWt5tFawVg=";
+  cargoHash = "sha256-A4v4i6U6BwUMNTI/TO7wTQvNVtQYKGiQfDXOCy8hFTE=";
+
+  # Workaround for errors in the tests
+  doCheck = false;
+
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  doInstallCheck = true;
+  versionCheckProgram = "${placeholder "out"}/bin/${meta.mainProgram}";
+  versionCheckProgramArg = [ "--version" ];
+
+  # TODO: Should adjust for their tag names
+  # passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Bash/POSIX-compatible shell implemented in Rust";
