8d03113893c7c5ef66088af7930a011c6c063b14
Kenichi Kamiya <kachick1@gmail.com>
2025-04-10 23:56:18 +0900

    fishnet: WIP better curl

--- pkgs/by-name/fi/fishnet/update.bash
+++ pkgs/by-name/fi/fishnet/update.bash
@@ -1,9 +1,9 @@
 new_version="$(
-	curl --silent "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" |
+	curl --fail --silent ${GITHUB_TOKEN:+-u ":$GITHUB_TOKEN"} "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" |
 		jq '.tag_name | ltrimstr("v")' --raw-output
 )"
-stockfish_revision="$(curl --silent "https://api.github.com/repos/lichess-org/fishnet/contents/Stockfish?ref=v$new_version" | jq .sha --raw-output)"
-stockfish_header="$(curl --silent "https://raw.githubusercontent.com/official-stockfish/Stockfish/$stockfish_revision/src/evaluate.h")"
+stockfish_revision="$(curl --fail --silent ${GITHUB_TOKEN:+-u ":$GITHUB_TOKEN"} "https://api.github.com/repos/lichess-org/fishnet/contents/Stockfish?ref=v$new_version" | jq .sha --raw-output)"
+stockfish_header="$(curl --fail --silent "https://raw.githubusercontent.com/official-stockfish/Stockfish/$stockfish_revision/src/evaluate.h")"
 new_nnue_big_file="$(echo "$stockfish_header" | grep --perl-regexp --only-matching 'EvalFileDefaultNameBig "\Knn-(\w+).nnue')"
 new_nnue_big_hash="$(nix hash to-sri --type sha256 "$(nix-prefetch-url --type sha256 "https://tests.stockfishchess.org/api/nn/${new_nnue_big_file}")")"
 new_nnue_small_file="$(echo "$stockfish_header" | grep --perl-regexp --only-matching 'EvalFileDefaultNameSmall "\Knn-(\w+).nnue')"
