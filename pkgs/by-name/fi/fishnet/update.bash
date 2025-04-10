142bfd16a75ad9b36ac49f9abb178673c016a9e8
Kenichi Kamiya <kachick1@gmail.com>
2025-04-10 23:40:47 +0900

    fishnet: WIP shorter vars

--- pkgs/by-name/fi/fishnet/update.bash
+++ pkgs/by-name/fi/fishnet/update.bash
@@ -1,21 +1,19 @@
-new_fishnet_version="$(
+new_version="$(
 	curl --silent "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" |
 		jq '.tag_name | ltrimstr("v")' --raw-output
 )"
-
-stockfish_revision="$(curl --silent "https://api.github.com/repos/lichess-org/fishnet/contents/Stockfish?ref=v$new_fishnet_version" | jq .sha --raw-output)"
+stockfish_revision="$(curl --silent "https://api.github.com/repos/lichess-org/fishnet/contents/Stockfish?ref=v$new_version" | jq .sha --raw-output)"
 stockfish_header="$(curl --silent "https://raw.githubusercontent.com/official-stockfish/Stockfish/$stockfish_revision/src/evaluate.h")"
+new_big_file="$(echo "$stockfish_header" | grep --perl-regexp --only-matching 'EvalFileDefaultNameBig "\Knn-(\w+).nnue')"
+new_big_hash="$(nix hash to-sri --type sha256 "$(nix-prefetch-url --type sha256 "https://tests.stockfishchess.org/api/nn/${new_big_file}")")"
+new_small_file="$(echo "$stockfish_header" | grep --perl-regexp --only-matching 'EvalFileDefaultNameSmall "\Knn-(\w+).nnue')"
+new_small_hash="$(nix hash to-sri --type sha256 "$(nix-prefetch-url --type sha256 "https://tests.stockfishchess.org/api/nn/${new_small_file}")")"
 
-new_nnueBig_file="$(echo "$stockfish_header" | grep --perl-regexp --only-matching 'EvalFileDefaultNameBig "\Knn-(\w+).nnue')"
-new_nnueBig_hash="$(nix hash to-sri --type sha256 "$(nix-prefetch-url --type sha256 "https://tests.stockfishchess.org/api/nn/${new_nnueBig_file}")")"
-new_nnueSmall_file="$(echo "$stockfish_header" | grep --perl-regexp --only-matching 'EvalFileDefaultNameSmall "\Knn-(\w+).nnue')"
-new_nnueSmall_hash="$(nix hash to-sri --type sha256 "$(nix-prefetch-url --type sha256 "https://tests.stockfishchess.org/api/nn/${new_nnueSmall_file}")")"
+update-source-version "$PNAME" "$new_version" --ignore-same-version --print-changes
 
-update-source-version "$PNAME" "$new_fishnet_version" --ignore-same-version --print-changes
-
-pfile_content="$(cat "$PFILE")"
-pfile_content="${pfile_content//"$NNUE_BIG_FILE"/"$new_nnueBig_file"}"
-pfile_content="${pfile_content//"$NNUE_BIG_HASH"/"$new_nnueBig_hash"}"
-pfile_content="${pfile_content//"$NNUE_SMALL_FILE"/"$new_nnueSmall_file"}"
-pfile_content="${pfile_content//"$NNUE_SMALL_HASH"/"$new_nnueSmall_hash"}"
-echo "$pfile_content" >"$PFILE"
+pkg_body="$(cat "$PKG_FILE")"
+pkg_body="${pkg_body//"$NNUE_BIG_FILE"/"$new_big_file"}"
+pkg_body="${pkg_body//"$NNUE_BIG_HASH"/"$new_big_hash"}"
+pkg_body="${pkg_body//"$NNUE_SMALL_FILE"/"$new_small_file"}"
+pkg_body="${pkg_body//"$NNUE_SMALL_HASH"/"$new_small_hash"}"
+echo "$pkg_body" >"$PKG_FILE"
