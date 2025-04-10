8c27a7af0a19b7ab7a1059e196a5bffb1162ebd8
Kenichi Kamiya <kachick1@gmail.com>
2025-04-10 21:21:19 +0900

    fishnet: WIP updater

--- pkgs/by-name/fi/fishnet/package.nix
+++ pkgs/by-name/fi/fishnet/package.nix
@@ -4,6 +4,12 @@
   fetchFromGitHub,
   fetchurl,
   versionCheckHook,
+  writeShellApplication,
+  curl,
+  jq,
+  ripgrep,
+  nix-prefetch-url,
+  common-updater-scripts,
 }:
 
 let
@@ -47,6 +53,48 @@ rustPlatform.buildRustPackage (finalAttrs: {
   doInstallCheck = true;
   versionCheckProgram = "${placeholder "out"}/bin/${finalAttrs.meta.mainProgram}";
   versionCheckProgramArg = "--version";
+
+  passthru = {
+    updateScript = lib.getExe (writeShellApplication {
+      name = "update-${finalAttrs.pname}";
+
+      runtimeInputs = [
+        curl
+        jq
+        ripgrep
+        nix-prefetch-url
+        common-updater-scripts
+      ];
+
+      text = ''
+        new_fishnet_version="$(
+          curl --silent https://api.github.com/repos/${finalAttrs.src.owner}/${finalAttrs.src.repo}/releases/latest | \
+            jq '.tag_name | ltrimstr("v")' --raw-output
+        )"
+
+        update-source-version "${finalAttrs.pname}" "$new_fishnet_version" --ignore-same-version --source-key="sources.fishnet" --print-changes
+
+        stockfish_revision="$(curl --silent "https://api.github.com/repos/lichess-org/fishnet/contents/Stockfish?ref=v$new_fishnet_version" | jq .sha --raw-output)"
+
+        stockfish_header="$(curl --silent "https://raw.githubusercontent.com/official-stockfish/Stockfish/$stockfish_revision/src/evaluate.h")"
+
+        new_nnueBig_version="$(echo "$stockfish_header" | rg 'EvalFileDefaultNameBig "nn-(\w+).nnue"' --only-matching --replace '$1')"
+        new_nnueBig_file="nn-''${new_nnueBig_version}.nnue"
+        new_nnueBig_hash="$(nix hash to-sri --type sha256 "$(nix-prefetch-url "https://tests.stockfishchess.org/api/nn/''${new_nnueBig_file}")")"
+
+        sed -i package.nix \
+            -e "s/${nnueBigFile}/''${new_nnueBig_file}/"
+
+        # new_nnueSmall_version="$(echo "$stockfish_header" | rg 'EvalFileDefaultNameSmall "nn-(\w+).nnue"' --only-matching --replace '$1')"
+
+        # update-source-version '${finalAttrs.pname}.passthru.sources.nnueBig' "$new_nnueBig_version" --ignore-same-version --file=pkgs/by-name/fi/${finalAttrs.pname}/nnue.nix --print-changes
+
+        # update-source-version '${finalAttrs.pname}.passthru.sources.nnueBig' "$new_nnueBig_version" --ignore-same-version --source-key="sources.nnueBig"
+
+        # update-source-version '${finalAttrs.pname}.passthru.sources.nnueSmall' "$new_nnueSmall_version" --ignore-same-version --source-key="sources.nnueSmall"
+      '';
+    });
+  };
 
   meta = with lib; {
     description = "Distributed Stockfish analysis for lichess.org";
