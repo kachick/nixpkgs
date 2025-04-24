cc2a8559ffbc24ab09086649b01df6e97f841e35
Kenichi Kamiya <kachick1@gmail.com>
2025-04-24 21:48:46 +0900

    ameba-ls: init

---
+++ pkgs/by-name/am/ameba-ls/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  fetchFromGitHub,
+  crystal,
+  unstableGitUpdater,
+# coreutils,
+}:
+
+crystal.buildCrystalPackage {
+  pname = "ameba-ls";
+  version = "unstable-2025-03-16";
+
+  src = fetchFromGitHub {
+    owner = "crystal-lang-tools";
+    repo = "ameba-ls";
+    rev = "5e52ab9e797829b64c44ed3738c3ee91c7e3f3b8";
+    hash = lib.fakeHash;
+  };
+
+  format = "make";
+  # installFlags = [ "INSTALL_BIN=${coreutils}/bin/install" ];
+
+  passthru.updateScript = unstableGitUpdater { };
+
+  meta = {
+    description = "Language server for the Ameba linter for Crystal language";
+    mainProgram = "ameba-ls";
+    homepage = "https://github.com/crystal-lang-tools/ameba-ls";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      kachick
+    ];
+  };
+}
