79f13186259f98c4b39811ac0f59c94b5e728f63
Kenichi Kamiya <kachick1@gmail.com>
2025-06-06 21:43:15 +0900

    ludii: init

--- pkgs/by-name/lu/ludii/package.nix
+++ pkgs/by-name/lu/ludii/package.nix
@@ -1,94 +1,75 @@
 {
   lib,
   stdenvNoCC,
-  fetchFromGitHub,
+  fetchurl,
+  # fetchFromGitHub,
   jdk,
   jre,
-  zip,
+  # zip,
   makeWrapper,
-  spleen,
   nix-update-script,
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
-  pname = "bitsnpicas";
-  version = "2.1";
+  pname = "ludii";
+  version = "1.3.14";
 
-  src = fetchFromGitHub {
-    owner = "kreativekorp";
-    repo = "bitsnpicas";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-hw7UuzesqpmnTjgpfikAIYyY70ni7BxjaUtHAPEdkXI=";
+  # src = fetchFromGitHub {
+  #   owner = "Ludeme";
+  #   repo = "Ludii";
+  #   tag = "v${finalAttrs.version}";
+  #   hash = "sha256-hw7UuzesqpmnTjgpfikAIYyY70ni7BxjaUtHAPEdkXI=";
+  # };
+
+  # GitHub repository https://github.com/Ludeme/Ludii/ does not have versioned tags.
+  src = fetchurl {
+    url = "https://ludii.games/downloads/Ludii-${finalAttrs.version}-src.jar";
+    hash = lib.fakeHash;
   };
 
   nativeBuildInputs = [
     jdk
-    zip
+    # zip
     makeWrapper
   ];
 
-  sourceRoot = "${finalAttrs.src.name}/main/java/BitsNPicas";
+  # sourceRoot = "${finalAttrs.src.name}/main/java/BitsNPicas";
 
-  installPhase =
-    ''
-      runHook preInstall
+  dontUnpack = true;
 
-      install -Dm444 BitsNPicas.jar "$out/share/java/bitsnpicas.jar"
-      install -Dm444 MapEdit.jar "$out/share/java/mapedit.jar"
-      install -Dm444 KeyEdit.jar "$out/share/java/keyedit.jar"
+  installPhase = ''
+    runHook preInstall
 
-      makeWrapper "${jre}/bin/java" "$out/bin/bitsnpicas" \
-        --add-flags "-jar $out/share/java/bitsnpicas.jar"
-      makeWrapper "${jre}/bin/java" "$out/bin/mapedit" \
-        --add-flags "-jar $out/share/java/mapedit.jar"
-      makeWrapper "${jre}/bin/java" "$out/bin/keyedit" \
-        --add-flags "-jar $out/share/java/keyedit.jar"
+    # mkdir -p $out/share/java
+    # mkdir -p $out/bin
+    install -Dm444 "$src" "$out/share/java/"
+    makeWrapper "${jre}/bin/java" $out/bin/bfg --add-flags "-cp $out/share/java/$jarName com.madgag.git.bfg.cli.Main"
 
-      install -Dm444 dep/bitsnpicas.png "$out/share/icons/hicolor/128x128/apps/bitsnpicas.png"
-      install -Dm444 dep/kbnp-icon.png "$out/share/icons/hicolor/512x512/apps/bitsnpicas.png"
-      install -Dm444 dep/mapedit-icon.png "$out/share/icons/hicolor/512x512/apps/mapedit.png"
-      install -Dm444 dep/keyedit-icon.png "$out/share/icons/hicolor/256x256/apps/keyedit.png"
-    ''
-    + lib.optionalString stdenvNoCC.hostPlatform.isLinux ''
-      mkdir -p "$out/share/applications/"
-      cp dep/*.desktop "$out/share/applications/"
-    ''
-    + ''
-      runHook postInstall
-    '';
+    makeWrapper "${jre}/bin/java" "$out/bin/Ludii" \
+      --add-flags "-jar $out/share/java/Ludii.jar"
 
-  postFixup = lib.optionalString stdenvNoCC.hostPlatform.isLinux ''
-    substituteInPlace "$out/share/applications/bitsnpicas.desktop" \
-      --replace-fail 'Exec=java -jar /usr/local/lib/bitsnpicas.jar' "Exec=$out/bin/bitsnpicas" \
-      --replace-fail 'Icon=/usr/share' "Icon=$out/share"
-    substituteInPlace "$out/share/applications/mapedit.desktop" \
-      --replace-fail 'Exec=java -jar /usr/local/lib/mapedit.jar' "Exec=$out/bin/mapedit" \
-      --replace-fail 'Icon=/usr/share' "Icon=$out/share"
-    substituteInPlace "$out/share/applications/keyedit.desktop" \
-      --replace-fail 'Exec=java -jar /usr/local/lib/keyedit.jar' "Exec=$out/bin/keyedit" \
-      --replace-fail 'Icon=/usr/share' "Icon=$out/share"
+    runHook postInstall
   '';
 
-  doInstallCheck = true;
-  installCheckPhase = ''
-    runHook preInstallCheck
+  # doInstallCheck = true;
+  # installCheckPhase = ''
+  #   runHook preInstallCheck
 
-    "$out/bin/bitsnpicas" convertbitmap -f psf "${spleen}/share/fonts/misc/spleen-8x16.bdf"
-    [[ -f Spleen.psf ]]
+  #   "$out/bin/bitsnpicas" convertbitmap -f psf "${spleen}/share/fonts/misc/spleen-8x16.bdf"
+  #   [[ -f Spleen.psf ]]
 
-    runHook postInstallCheck
-  '';
+  #   runHook postInstallCheck
+  # '';
 
   passthru = {
     updateScript = nix-update-script { };
   };
 
   meta = {
-    description = "Bitmap and emoji font creation and conversion tools";
-    homepage = "https://github.com/kreativekorp/bitsnpicas";
-    # Written in https://github.com/kreativekorp/bitsnpicas/blob/v2.1/main/java/BitsNPicas/LICENSE
-    license = lib.licenses.mpl11;
-    mainProgram = "bitsnpicas";
+    description = "General game system";
+    homepage = "http://ludii.games";
+    license = lib.licenses.cc-by-nc-nd-40;
+    mainProgram = "Ludii";
     maintainers = with lib.maintainers; [
       kachick
     ];
