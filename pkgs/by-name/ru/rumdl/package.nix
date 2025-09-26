fb6b93b9c2ed71cb2fec3b97f6ee424cd8202530
Kenichi Kamiya <kachick1@gmail.com>
2025-09-26 15:22:41 +0900

    rumdl: init at 0.0.146

---
+++ pkgs/by-name/ru/rumdl/package.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  versionCheckHook,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "rumdl";
+  version = "0.0.146";
+
+  src = fetchFromGitHub {
+    owner = "rvben";
+    repo = "rumdl";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-0aeBVapWJ+1Y/D7WA1oYACD8Mr0tSLiv+ojJhZ3i3pI=";
+  };
+
+  cargoHash = "sha256-yrDf0+QMBzHhGNBTZp9gQNqH3USagpJklRDrUvjHnbw=";
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  versionCheckProgramArg = "--version";
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
+    description = "Markdown Linter";
+    homepage = "https://github.com/rvben/rumdl";
+    changelog = "https://github.com/rvben/rumdl/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      kachick
+    ];
+    mainProgram = "rumdl";
+  };
+})
