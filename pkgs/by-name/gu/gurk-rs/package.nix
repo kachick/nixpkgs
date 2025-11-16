94296809207e518fb958caf1cbf3a4f1e973ac3c
Kenichi Kamiya <kachick1@gmail.com>
2025-11-16 17:41:45 +0900

    gurk-rs: 0.6.4 -> 0.7.2-unstable-2025-11-16
    Diff: https://github.com/boxdot/gurk-rs/compare/v0.7.2...kachick:gurk-rs:gurk-rs-0.7.2-sync-rust-edition

--- pkgs/by-name/gu/gurk-rs/package.nix
+++ pkgs/by-name/gu/gurk-rs/package.nix
@@ -15,20 +15,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gurk-rs";
-  version = "0.6.4";
+  version = "0.7.2-unstable-2025-11-16";
 
   src = fetchFromGitHub {
-    owner = "boxdot";
+    # The Nixpkgs version update seems stuck since the changes in this commit range were added: https://github.com/whisperfish/presage/compare/473c70d...2acc5328a.
+    # I guess the problem is that Nixpkgs does not handle the Rust edition difference (presage-2021 vs presage-store-sqlite-2024, gurk-rs-2024) correctly.
+    # This is my temporary fork to avoid the issue.
+    owner = "kachick";
     repo = "gurk-rs";
-    tag = "v${version}";
-    hash = "sha256-1vnyzKissOciLopWzWN2kmraFevYW/w32KVmP8qgUM4=";
+    rev = "7eb62779ce32bb3e380e2e4e4dca1df7bf5eaade";
+    hash = "sha256-uPE7qXQlzkqkdxyHIqnXs59lYiwaYARi99Kwtz3kuko=";
   };
 
   postPatch = ''
     rm .cargo/config.toml
   '';
 
-  cargoHash = "sha256-PCeiJYeIeMgKoQYiDI6DPwNgJcSxw4gw6Ra1YmqsNys=";
+  cargoHash = "sha256-Naykepi3t36z2pcBB/+emcDYRexJtW9EJVGw17znKOo=";
 
   nativeBuildInputs = [
     protobuf
@@ -57,7 +60,9 @@ rustPlatform.buildRustPackage rec {
   versionCheckProgram = "${placeholder "out"}/bin/${meta.mainProgram}";
   versionCheckProgramArg = "--version";
 
-  passthru.updateScript = nix-update-script { };
+  passthru.updateScript = nix-update-script {
+    extraArgs = [ "--version=branch=gurk-rs-0.7.2-sync-rust-edition" ];
+  };
 
   meta = with lib; {
     description = "Signal Messenger client for terminal";
@@ -65,5 +70,10 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/boxdot/gurk-rs";
     license = licenses.agpl3Only;
     maintainers = with maintainers; [ devhell ];
+
+    # On Darwin, this package requires sandbox-relaxed to build.
+    # If the sandbox is enabled, `fetch-cargo-vendor-util` causes errors.
+    # This issue may be related to: https://github.com/NixOS/nixpkgs/issues/394972
+    # broken = stdenv.hostPlatform.isDarwin;
   };
 }
