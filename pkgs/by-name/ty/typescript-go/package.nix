5bea045023672a00f7c7b62e0da03a0272511c0e
Kenichi Kamiya <kachick1@gmail.com>
2026-01-15 12:57:52 +0900

    typescript-go: better version

--- pkgs/by-name/ty/typescript-go/package.nix
+++ pkgs/by-name/ty/typescript-go/package.nix
@@ -2,13 +2,17 @@
   lib,
   buildGo125Module,
   fetchFromGitHub,
+  _experimental-update-script-combinators,
   nix-update-script,
+  writeShellApplication,
+  gnugrep,
+  common-updater-scripts,
 }:
 
 let
   buildGoModule = buildGo125Module;
 in
-buildGoModule {
+buildGoModule (finalAttrs: {
   pname = "typescript-go";
   version = "0-unstable-2026-01-14";
 
@@ -44,10 +48,54 @@ buildGoModule {
   '';
 
   passthru = {
-    updateScript = nix-update-script {
-      extraArgs = [ "--version=branch" ];
-    };
+    updateScript = _experimental-update-script-combinators.sequence [
+      (nix-update-script {
+        extraArgs = [ "--version=branch" ];
+      })
+
+      (lib.getExe (writeShellApplication {
+        name = "${finalAttrs.pname}-update-script";
+
+        runtimeInputs = [
+          # curl
+          # jq
+          common-updater-scripts
+          gnugrep
+        ];
+
+        text = ''
+          new_major_minor="$(grep --only-matching --perl-regexp '(?<=var version =").*(?=")' '${finalAttrs.src}/internal/core/version.go')"
+          suffix="$(grep '(-unstable-[0-9-]+)' --only-matching --perl-regexp  <<< '${finalAttrs.version}')"
+          update-source-version '${finalAttrs.pname}' "$new_major_minor$suffix"
+          # sed -i -E "s/${finalAttrs.version}/$new_version/g" '${./package.nix}'
+        '';
+      }))
+    ];
   };
+
+  # passthru = {
+  #   updateScript = lib.getExe (writeShellApplication {
+  #     name = "lichess-bot-update-script";
+
+  #     runtimeInputs = [
+  #       curl
+  #       jq
+  #       common-updater-scripts
+  #     ];
+
+  #     text = ''
+  #       commit_msg='^Auto update version to (?<ver>[0-9.]+)$'
+  #       commit="$(
+  #         curl -s 'https://api.github.com/repos/lichess-bot-devs/lichess-bot/commits?path=lib/versioning.yml' | \
+  #         jq -c "map(select(.commit.message | test(\"$commit_msg\"))) | first"
+  #       )"
+  #       rev="$(jq -r '.sha' <<< "$commit")"
+  #       version="$(jq -r ".commit.message | capture(\"$commit_msg\") | .ver" <<< "$commit")"
+
+  #       update-source-version lichess-bot "$version" --rev="$rev"
+  #     '';
+  #   });
+  # };
 
   meta = {
     description = "Go implementation of TypeScript";
@@ -58,4 +106,4 @@ buildGoModule {
     ];
     mainProgram = "tsgo";
   };
-}
+})
